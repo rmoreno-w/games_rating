37de4a7b8fb8d94788f0915bc4a87c578250ddb5
Matheus Menezes <matheushm1798@gmail.com>
2022-12-15 08:01:01 -0300

    refactor: change constant name

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -4,24 +4,24 @@ const jwt = require("jsonwebtoken");
 
 exports.auth = async (request, response) => {
   let { email, password } = request.body;
-  const employee = await user.findByEmail(email);
+  const userExists = await user.findByEmail(email);
 
-  if (!employee) {
+  if (!userExists) {
     return response
       .status(401)
-      .json({ error: "There is no employee with this email" });
+      .json({ error: "There is no userExists with this email" });
   }
 
-  let isValidPassword = password === employee.password ? true : false;
+  let isValidPassword = password === userExists.password ? true : false;
 
   if (!isValidPassword) {
     return response.status(401).json({ error: "The password is incorrect" });
   }
 
   const token = jwt.sign({}, auth.jwt.secret, {
-    subject: employee.id,
+    subject: userExists.id,
     expiresIn: auth.jwt.expiresIn,
   });
 
-  return response.status(200).json({ employee, token });
+  return response.status(200).json({ userExists, token });
 };
